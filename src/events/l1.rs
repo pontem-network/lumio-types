90ebd2f12927a51a3d7ea50941a19eabafa592ed
rig410 <dmitryyakushev@dfinance.co>
2024-07-10 20:37:08 +0300

    feat: Refactor event modules and add Transfer struct
    Refactored the event modules `l1.rs` and `l2.rs` to use a common `Transfer` struct for deposit and withdrawal events. This improves code organization and reduces duplication. Also added the `Transfer` struct to the `mod.rs` file for shared usage.

--- src/events/l1.rs
+++ src/events/l1.rs
@@ -1,32 +1,8 @@
 use serde::{Deserialize, Serialize};
 
-use crate::Address;
+use super::Transfer;
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub enum L1Event {
-    Deposit(TxMint),
-}
-
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
-pub struct TxMint {
-    pub account: Address,
-    pub amount: u64,
-}
-
-#[cfg(test)]
-mod test {
-    use rand::random;
-
-    use super::*;
-
-    #[test]
-    fn test_serde_tx_mint() {
-        let tx = super::TxMint {
-            account: Address::from(random::<[u8; 32]>()),
-            amount: 100,
-        };
-        let encoded_tx = serde_json::to_string(&tx).unwrap();
-        let decoded_tx: super::TxMint = serde_json::from_str(&encoded_tx).unwrap();
-        assert_eq!(tx, decoded_tx);
-    }
+    Deposit(Transfer),
 }
--- src/events/l2.rs
+++ src/events/l2.rs
@@ -1,4 +1,8 @@
 use serde::{Deserialize, Serialize};
 
+use super::Transfer;
+
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
-pub enum L2Event {}
+pub enum L2Event {
+    Withdrawal(Transfer),
+}
