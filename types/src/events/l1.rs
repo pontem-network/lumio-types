b554b308f3b99a62504e3e6ea890a2225e8b02c9
Rail Khusnutdinov <rail@33.run>
2024-07-30 18:24:07 +0300

    add strum for L1Event

--- types/src/events/l1.rs
+++ types/src/events/l1.rs
@@ -1,8 +1,9 @@
 use serde::{Deserialize, Serialize};
+use strum::IntoStaticStr;
 
 use super::Bridge;
 
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize, IntoStaticStr)]
 pub enum L1Event {
     Deposit(Bridge),
 }
