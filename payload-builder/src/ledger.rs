ba1ca7363a648a8178e1720f284c68a9d77b0a11
rig410 <dmitryyakushev@dfinance.co>
2024-07-30 11:10:12 +0300

    remove async-trait

--- payload-builder/src/ledger.rs
+++ payload-builder/src/ledger.rs
@@ -1,14 +1,17 @@
+use std::future::Future;
+
 use anyhow::Error;
-use async_trait::async_trait;
 use lumio_types::Slot;
 
 use crate::SlotArtifact;
 
-#[async_trait]
 pub trait Ledger {
     /// Get the current slot.
-    async fn get_current_slot(&self) -> Result<Slot, Error>;
+    fn get_current_slot(&self) -> impl Future<Output = Result<Slot, Error>> + Send + 'static;
     /// Get the slot artifact by slot id. If the slot with slot_id skipped, return next slot.
     /// If the slot with slot_id has not been produced yet, return None.
-    async fn get_slot(&mut self, slot_id: Slot) -> Result<Option<SlotArtifact>, Error>;
+    fn get_slot(
+        &mut self,
+        slot_id: Slot,
+    ) -> impl Future<Output = Result<Option<SlotArtifact>, Error>> + Send + 'static;
 }
