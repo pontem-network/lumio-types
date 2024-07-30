b7075f493c9d2b212b650cb73da275e7c5781343
Rail Khusnutdinov <rail@33.run>
2024-07-30 17:20:40 +0300

    add strum for L2Event

--- types/src/events/l2.rs
+++ types/src/events/l2.rs
@@ -1,8 +1,9 @@
 use serde::{Deserialize, Serialize};
+use strum::IntoStaticStr;
 
 use super::Bridge;
 
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize, IntoStaticStr)]
 pub enum L2Event {
     Withdrawal(Bridge),
 }
