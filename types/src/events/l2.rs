671d2045a8955bcdc99da4154e70100223d7eaa3
rig410 <dmitryyakushev@dfinance.co>
2024-08-13 19:03:06 +0300

    feat: Add SplL1Event and SplL2Event for bridging and transferring tokens

--- types/src/events/l2.rs
+++ types/src/events/l2.rs
@@ -6,4 +6,18 @@ use super::Bridge;
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize, IntoStaticStr)]
 pub enum L2Event {
     Withdrawal(Bridge),
+    Spl(SplL2Event),
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize, IntoStaticStr)]
+pub enum SplL2Event {
+    Bridged {
+        l1_mint: H256,
+        l2_mint: H256,
+    },
+    Transfer {
+        l1_mint: H256,
+        to: H256,
+        amount: u64,
+    },
 }
