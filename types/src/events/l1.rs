230d94e035945c54506df12e1e75226b23463b42
rig410 <dmitryyakushev@dfinance.co>
2024-08-14 21:26:18 +0300

    feat: Add TokenMetadata to SplL1Event

--- types/src/events/l1.rs
+++ types/src/events/l1.rs
@@ -8,7 +8,7 @@ use super::Bridge;
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize, IntoStaticStr)]
 pub enum L1Event {
     Deposit(Bridge),
-    Spl(SplL1Event)
+    Spl(SplL1Event),
 }
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize, IntoStaticStr)]
@@ -16,10 +16,18 @@ pub enum SplL1Event {
     Bridge {
         l1_mint: H256,
         decimals: u8,
+        metadata: Option<TokenMetadata>,
     },
     Transfer {
         l1_mint: H256,
         to: H256,
         amount: u64,
     },
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct TokenMetadata {
+    pub name: String,
+    pub symbol: String,
+    pub uri: String,
 }
