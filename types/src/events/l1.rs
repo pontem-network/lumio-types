081a377df461232f5c7d7b71725029c81cc2a9fb
rig410 <dmitryyakushev@dfinance.co>
2024-08-14 22:22:31 +0300

    refactor: Remove metadata field from TokenMetadata struct

--- types/src/events/l1.rs
+++ types/src/events/l1.rs
@@ -16,7 +16,6 @@ pub enum SplL1Event {
     Bridge {
         l1_mint: H256,
         decimals: u8,
-        metadata: Option<TokenMetadata>,
     },
     Transfer {
         l1_mint: H256,
@@ -25,9 +24,3 @@ pub enum SplL1Event {
     },
 }
 
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
-pub struct TokenMetadata {
-    pub name: String,
-    pub symbol: String,
-    pub uri: String,
-}
