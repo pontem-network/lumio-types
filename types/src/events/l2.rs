5ff9caeed1bedc59cec1fcb354e35a9574b7faa2
rig410 <dmitryyakushev@dfinance.co>
2024-08-16 22:00:04 +0300

    feat: Remove bridged event from SplL2Event

--- types/src/events/l2.rs
+++ types/src/events/l2.rs
@@ -13,10 +13,6 @@ pub enum L2Event {
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize, IntoStaticStr)]
 pub enum SplL2Event {
-    Bridged {
-        l1_mint: H256,
-        l2_mint: H256,
-    },
     Transfer {
         l1_mint: H256,
         to: H256,
