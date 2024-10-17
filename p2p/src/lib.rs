3bc3c32e6275d1311c03341a5b2f77072631246d
vladimirovmm <vladimirov.m.m@mail.ru>
2024-10-17 16:23:35 +0700

    refactor: the creation of "swarm" has been moved to a separate function for reuse in the client

--- p2p/src/lib.rs
+++ p2p/src/lib.rs
@@ -2,8 +2,10 @@
 
 use eyre::{Result, WrapErr};
 use futures::prelude::*;
-use libp2p::gossipsub;
+use libp2p::identity::Keypair;
 use libp2p::multiaddr::Multiaddr;
+use libp2p::pnet::PreSharedKey;
+use libp2p::{gossipsub, Swarm};
 use lumio_types::events::l2::EngineActions;
 use lumio_types::p2p::{SlotAttribute, SlotPayloadWithEvents};
 use lumio_types::Slot;
@@ -146,44 +148,7 @@ impl Node {
             psk,
         } = cfg;
 
-        let mut swarm = libp2p::SwarmBuilder::with_existing_identity(keypair)
-            .with_tokio()
-            .with_other_transport(|key| {
-                use libp2p::Transport;
-
-                libp2p::tcp::tokio::Transport::new(libp2p::tcp::Config::default().nodelay(true))
-                    .and_then(move |socket, _| libp2p::pnet::PnetConfig::new(psk).handshake(socket))
-                    .upgrade(libp2p::core::transport::upgrade::Version::V1Lazy)
-                    .authenticate(
-                        libp2p::noise::Config::new(key).expect("Failed to make noise config"),
-                    )
-                    .multiplex(libp2p::yamux::Config::default())
-            })?
-            .with_behaviour(|key| {
-                // To content-address message, we can take the hash of message and use it as an ID.
-                let message_id_fn = |message: &gossipsub::Message| {
-                    let mut s = std::hash::DefaultHasher::new();
-                    message.hash(&mut s);
-                    gossipsub::MessageId::from(s.finish().to_string())
-                };
-
-                // Set a custom gossipsub configuration
-                let gossipsub_config = gossipsub::ConfigBuilder::default()
-                    .heartbeat_interval(Duration::from_secs(10)) // This is set to aid debugging by not cluttering the log space
-                    .validation_mode(gossipsub::ValidationMode::Strict) // This sets the kind of message validation. The default is Strict (enforce message signing)
-                    .message_id_fn(message_id_fn) // content-address messages. No two messages of the same content will be propagated.
-                    .build()?;
-
-                // build a gossipsub network behaviour
-                let gossipsub = gossipsub::Behaviour::new(
-                    gossipsub::MessageAuthenticity::Signed(key.clone()),
-                    gossipsub_config,
-                )?;
-
-                Ok(LumioBehaviour { gossipsub })
-            })?
-            .with_swarm_config(|c| c.with_idle_connection_timeout(Duration::from_secs(60)))
-            .build();
+        let mut swarm = create_swarm_lumio(keypair, psk)?;
 
         for a in listen_on {
             swarm.listen_on(a).context("Failed to listen on address")?;
@@ -543,4 +508,44 @@ pub(crate) mod topics {
             IdentTopic::new(format!("/lumio/v1/sol/engine/since/{}", self.0))
         }
     }
+}
+
+fn create_swarm_lumio(keypair: Keypair, psk: PreSharedKey) -> eyre::Result<Swarm<LumioBehaviour>> {
+    let swarm = libp2p::SwarmBuilder::with_existing_identity(keypair)
+        .with_tokio()
+        .with_other_transport(|key| {
+            use libp2p::Transport;
+
+            libp2p::tcp::tokio::Transport::new(libp2p::tcp::Config::default().nodelay(true))
+                .and_then(move |socket, _| libp2p::pnet::PnetConfig::new(psk).handshake(socket))
+                .upgrade(libp2p::core::transport::upgrade::Version::V1Lazy)
+                .authenticate(libp2p::noise::Config::new(key).expect("Failed to make noise config"))
+                .multiplex(libp2p::yamux::Config::default())
+        })?
+        .with_behaviour(|key| {
+            // To content-address message, we can take the hash of message and use it as an ID.
+            let message_id_fn = |message: &gossipsub::Message| {
+                let mut s = std::hash::DefaultHasher::new();
+                message.hash(&mut s);
+                gossipsub::MessageId::from(s.finish().to_string())
+            };
+
+            // Set a custom gossipsub configuration
+            let gossipsub_config = gossipsub::ConfigBuilder::default()
+                .heartbeat_interval(Duration::from_secs(10)) // This is set to aid debugging by not cluttering the log space
+                .validation_mode(gossipsub::ValidationMode::Strict) // This sets the kind of message validation. The default is Strict (enforce message signing)
+                .message_id_fn(message_id_fn) // content-address messages. No two messages of the same content will be propagated.
+                .build()?;
+
+            // build a gossipsub network behaviour
+            let gossipsub = gossipsub::Behaviour::new(
+                gossipsub::MessageAuthenticity::Signed(key.clone()),
+                gossipsub_config,
+            )?;
+
+            Ok(LumioBehaviour { gossipsub })
+        })?
+        .with_swarm_config(|c| c.with_idle_connection_timeout(Duration::from_secs(60)))
+        .build();
+    Ok(swarm)
 }
