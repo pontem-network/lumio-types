a86dc90bb93c570b2be88e60cee4c2367e485e22
i1i1 <vanyarybin1@live.ru>
2024-10-25 18:57:03 +0300

    Remove sync status from slot attributes

--- rpc/tests/integration/mod.rs
+++ rpc/tests/integration/mod.rs
@@ -17,19 +17,6 @@ async fn simple() {
     let attrs = SlotAttribute {
         events: vec![],
         slot_id: 11,
-        sync_status: None,
-    };
-    lumio_attrs_sub.send(attrs.clone()).await.unwrap();
-    assert_eq!(sol_attrs.next().await.unwrap().unwrap(), attrs);
-
-    let mut lumio_attrs = lumio.handle_attrs_since().await.unwrap();
-    let mut sol_attrs = sol.subscribe_lumio_attrs_since(10).await.unwrap();
-    let (slot, mut lumio_attrs_sub) = lumio_attrs.next().await.unwrap();
-    assert_eq!(slot, 10);
-    let attrs = SlotAttribute {
-        events: vec![],
-        slot_id: 11,
-        sync_status: None,
     };
     lumio_attrs_sub.send(attrs.clone()).await.unwrap();
     assert_eq!(sol_attrs.next().await.unwrap().unwrap(), attrs);
