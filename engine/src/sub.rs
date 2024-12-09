e49e7ac9fe2cb1261e3712f0ca7972c8f29b9d4f
rig410 <dmitryyakushev@dfinance.co>
2024-12-09 11:31:43 +0300

    Introduce SlotEventsSub for handling EngineEvents and refactor SlotSub

--- engine/src/sub.rs
+++ engine/src/sub.rs
@@ -2,16 +2,17 @@ use crate::ledger::Ledger;
 use eyre::{eyre, Error};
 use futures::Sink;
 use futures::SinkExt;
+use lumio_types::p2p::EngineEvents;
 use lumio_types::{p2p::SlotPayloadWithEvents, Slot};
 use std::sync::Arc;
 
-pub struct SlotSub<L, S> {
+pub struct SlotPayloadSub<L, S> {
     slot: Slot,
     ledger: Arc<L>,
     sender: S,
 }
 
-impl<L, S> SlotSub<L, S>
+impl<L, S> SlotPayloadSub<L, S>
 where
     L: Ledger + Send + Sync + 'static,
     S: Sink<SlotPayloadWithEvents> + Unpin + 'static,
@@ -29,6 +30,39 @@ where
             let ledger = self.ledger.clone();
             let slot = self.slot;
             let slot = tokio::task::spawn_blocking(move || ledger.get_payload(slot)).await??;
+            self.sender
+                .send(slot)
+                .await
+                .map_err(|_| eyre!("Failed to send slot payload"))?;
+            self.slot += 1;
+        }
+    }
+}
+
+pub struct SlotEventsSub<L, S> {
+    slot: Slot,
+    ledger: Arc<L>,
+    sender: S,
+}
+
+impl<L, S> SlotEventsSub<L, S>
+where
+    L: Ledger + Send + Sync + 'static,
+    S: Sink<EngineEvents> + Unpin + 'static,
+{
+    pub fn new(slot: Slot, ledger: Arc<L>, sender: S) -> Self {
+        Self {
+            slot,
+            ledger,
+            sender,
+        }
+    }
+
+    pub async fn run(mut self) -> Result<(), Error> {
+        loop {
+            let ledger = self.ledger.clone();
+            let slot = self.slot;
+            let slot = tokio::task::spawn_blocking(move || ledger.get_events(slot)).await??;
             self.sender
                 .send(slot)
                 .await
