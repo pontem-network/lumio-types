4d5adbd19216b43c65961ab7d4763e44bf99375d
rig410 <dmitryyakushev@dfinance.co>
2024-12-09 22:18:27 +0300

    add any to to type

--- types/src/to.rs
+++ types/src/to.rs
@@ -7,6 +7,7 @@ pub enum To<T> {
     OpSol(T),
     OpMove(T),
     Lumio(T),
+    Any(T),
 }
 
 impl<T> To<T> {
@@ -15,6 +16,28 @@ impl<T> To<T> {
             To::OpSol(inner) => inner,
             To::OpMove(inner) => inner,
             To::Lumio(inner) => inner,
+            To::Any(inner) => inner,
+        }
+    }
+
+    pub fn for_lumio(self) -> Option<T> {
+        match self {
+            To::OpSol(_) | To::OpMove(_) => None,
+            To::Lumio(inner) | To::Any(inner) => Some(inner),
+        }
+    }
+
+    pub fn for_op_sol(self) -> Option<T> {
+        match self {
+            To::Lumio(_) | To::OpMove(_) => None,
+            To::OpSol(inner) | To::Any(inner) => Some(inner),
+        }
+    }
+
+    pub fn for_op_move(self) -> Option<T> {
+        match self {
+            To::Lumio(_) | To::OpSol(_) => None,
+            To::OpMove(inner) | To::Any(inner) => Some(inner),
         }
     }
 }
