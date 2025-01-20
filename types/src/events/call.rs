ec33265b4020cf7e210484a91a016bac912926fa
rig410 <dmitryyakushev@dfinance.co>
2025-01-20 16:13:24 +0300

    add attach_amount field to MoveCall and SolanaCall structs

--- types/src/events/call.rs
+++ types/src/events/call.rs
@@ -16,6 +16,7 @@ pub struct MoveCall {
     pub function: String,
     pub tp_args: Vec<String>,
     pub args: Vec<MoveValue>,
+    pub attach_amount: u64,
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
@@ -24,6 +25,7 @@ pub struct SolanaCall {
     pub instruction: Vec<u8>,
     pub on_fail: Option<Box<MoveCall>>,
     pub os_success: Option<Box<MoveCall>>,
+    pub attach_amount: u64,
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
